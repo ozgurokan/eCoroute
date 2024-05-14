0e9e64b4c977d10c48464f2646dbad3a5b6f4c03
Özgür Okan Özdal <ozgurokanozdal@gmail.com>
2024-05-14 12:24:06 +0300

    map done and prev next buttons done

--- src/pages/GetRoute.jsx
+++ src/pages/GetRoute.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import {Flex} from "@chakra-ui/react"
+import React,{useEffect,useState} from 'react'
+import {Flex,Box} from "@chakra-ui/react"
 import Map from '../components/map/MapBox'
+import axios from "axios"
 
 function GetRoute() {
+
+  const [actualData,setActualData] = useState(null)
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/api/v1/optimum-route/1").then((response) => {
+      setActualData(response.data)
+    }).catch((err) => console.log(err))
+  },[])
+
   return (
     <Flex width={"100%"} h="80vh" bgColor={"gray.800"}>
-       <Map></Map>
+      {
+        !actualData && <Box>Loading</Box>
+      }
+      {
+        actualData && <Map data_list={actualData}> </Map> 
+      }
     </Flex>
   )
 }
